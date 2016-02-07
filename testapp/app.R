51ca884ed8ecb0a5e66fc8a34d319e647fc7aed0
harveyl888 <harvey.lieberman@gmail.com>
2016-02-07 12:33:19 -0500

    Corrected css components.

--- testapp/app.R
+++ testapp/app.R
@@ -6,7 +6,7 @@ server <- function(input, output, session) {
 }
 
 prog.labels <- c('read file', 'data reduction', 'normalization', 'trends', 'visualization')
-prog.status <- c('waiting', 'waiting', 'inactive', 'waiting', 'waiting')
+prog.status <- c('active', 'completed', 'waiting', 'inactive', 'failed')
 
 ui <- fluidPage(
   titlePanel(title = 'progressList example'),
